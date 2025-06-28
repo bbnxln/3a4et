2904fbede82803113e59e5e19ca67df2832bf1a2
nasya <akostina2007@mail.ru>
2025-06-28 14:15:09 +0300

    Add files via upload

---
+++ abbr.py
@@ -0,0 +1,9 @@
+def make_abbreviation(phrase):
+    words = phrase.split()
+    abbreviation = ''.join(word[0] for word in words if word[0].isalpha())
+    return abbreviation.upper()
+
+# Ввод от пользователя
+user_input = input("Введите фразу: ")
+result = make_abbreviation(user_input)
+print("Аббревиатура:", result)
